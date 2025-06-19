a882954479d1a093fa33fc56c665c184a9395d92
I578070 <mohit.naskar@sap.com>
2025-06-19 21:59:45 +0530

    Django

---
+++ Django/Lesson01/myproject/posts/urls.py
@@ -0,0 +1,10 @@
+from aem import app
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('',views.posts_list,name='list'),
+    path('<slug:slug>', views.post_page, name='posts_page'),
+]

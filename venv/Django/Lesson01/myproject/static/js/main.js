6656cffaa0e1875253c8f8ee87e70f261515b41d
I578070 <mohit.naskar@sap.com>
2026-04-24 17:47:58 +0530

    Add gitignore and stop tracking venv

--- venv/Django/Lesson01/myproject/static/js/main.js
+++
@@ -1,1 +0,0 @@
-console.log('THis is js from static')
--- venv/python/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def test(x):
-#     if x > 100:
-#         raise ValueTooHighError("Value is too high")
-#     else:
-#         return x
-    pass
